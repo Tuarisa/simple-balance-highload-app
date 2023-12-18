a7888dc2a00b116e22fcbba1d8780a94d2bc791f
Tuarisa <tuarisa@gmail.com>
2023-12-18 11:36:44 +0100

    Refactor userValidator updateUserBalanceSchema to prevent zero amount

--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -2,5 +2,5 @@ import Joi from 'joi';
 
 export const updateUserBalanceSchema = Joi.object({
   userId: Joi.number().required(),
-  amount: Joi.number().required()
+  amount: Joi.number().invalid(0).required()
 });
